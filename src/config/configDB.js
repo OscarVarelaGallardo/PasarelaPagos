e7363b8edc556d950404a8b791364627e806bf21
Oscar Varela <oscar.varela@tilatina.com>
2025-06-02 22:54:27 -0600

    Improve database connection logging
    Adds connection success and error messages to enhance debugging.
    
    Logs the database connection URL upon successful connection and outputs an error message if the connection fails, aiding in troubleshooting.

--- src/config/configDB.js
+++ src/config/configDB.js
@@ -5,9 +5,10 @@ import mongoose from 'mongoose'
     try {
         const { connection } = await mongoose.connect(process.env.MONGO_URI)
         const url = `${connection.host}:${connection.port}`
-     
+        console.log(`Base de datos conectada en: ${url}`)
     } catch (error) {
         //Terminar proceso asincrono
+        console.error('Error al conectar a la base de datos:', error.message)
         process.exit(1)
     }
 }
